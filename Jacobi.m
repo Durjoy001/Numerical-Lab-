a4d68e68a712960a3c4ac72b4ce89279d3bf2236
Durjoy Dey <43565286+Durjoy001@users.noreply.github.com>
2019-10-29 23:19:12 +0600

    Create Jacobi.m

---
+++ Jacobi.m
@@ -0,0 +1,22 @@
+function retval =GaussJacobi(A, b)
+  [r c] = size(A);
+  b = b';
+  n = length(b); 
+  x = zeros(n, 1);
+  xnew = zeros(n, 1);
+  x(:) = 0
+  
+  for iteration = 1:20
+    for i = 1:n
+        sum = 0;
+        for j = 1:n
+          if i ~= j
+          sum = sum + A(i, j)*x(j);
+          endif
+        endfor
+        xnew(i) = -1/A(i, i) * (sum - b(i));
+      endfor
+      x = xnew;
+    endfor
+retval = [x'];
+endfunction
