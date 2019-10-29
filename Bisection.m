20d079f6893bad2ab8292084eb177022b2ed40b9
Durjoy Dey <43565286+Durjoy001@users.noreply.github.com>
2019-10-29 23:20:44 +0600

    Update Bisection.m

--- Bisection.m
+++ Bisection.m
@@ -1,15 +1,13 @@
-function value = Bisection(f, A, B)
-  c(1) = (A(1)+B(1))/2;
-  fa(1) = f(A(1));
-  fb(1) = f(B(1));
+function value = Bisection(f, a, b)
+  A(1) = a;
+  B(1) = b;
+  c(1) = (a+b)/2;
+  fa(1) = f(a);
+  fb(1) = f(b);
   fc(1) = f(c(1));
   
   for i = 2:20
-    fa(i) = f(A(i-1));
-    fb(i) = f(B(i-1));
-    c(i) = (A(i-1)+B(i-1))/2;
-    fc(i) = f(c(i));
-    #disp(sprintf('%0.6f  %0.6f  %0.6f  %0.6f  %0.6f  %0.6f',a, b, fa, fb, c, fc))
+
     if fa(i-1)*fc(i-1) < 0 
       B(i) = c(i-1);
       A(i) = A(i-1);
@@ -17,6 +15,11 @@ function value = Bisection(f, A, B)
       A(i) = c(i-1);
       B(i) = B(i-1);
     endif
+    
+    fa(i) = f(A(i));
+    fb(i) = f(B(i));
+    c(i) = (A(i)+B(i))/2;
+    fc(i) = f(c(i));
     
   endfor
   value = [A' B' fa' fb' c' fc'];
